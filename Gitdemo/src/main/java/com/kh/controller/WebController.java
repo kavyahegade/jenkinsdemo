6484b5761462b2b9a565028aedd638473be26c48
kavyahegade <kavyashreeubdt@gmail.com>
2020-11-09 12:07:53 +0530

    First Commit

---
+++ Gitdemo/src/main/java/com/kh/controller/WebController.java
@@ -0,0 +1,15 @@
+package com.kh.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+	
+	
+	@GetMapping("/hi")
+	public String sayHello() {
+		   return "Hello World";
+	}
+
+}
